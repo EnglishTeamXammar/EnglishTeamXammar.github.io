af0f37ff1d37f9533f11692b28b6cd0400a10785
EnglishTeamXammar <d40592940p@perueduca.edu.pe>
2026-04-30 10:00:00 -0500

    Update contenido.js

--- grado4/contenido/contenido.js
+++ grado4/contenido/contenido.js
@@ -1,54 +1,25 @@
 const contenido = [
     {
-        tipo:"video",
-        src:"https://youtu.be/-lc5lKJZSGE",
-        titulo:"Si Tú Tienes - If You’re Happy ♫ "
-    },
-    {
         tipo: "video",
         src: "https://youtu.be/jrIBc2sbm6M",
-        titulo: "Padre Nuestro en inglés"
+        titulo: "Padre Nuestro de lento a rapido"
     },
     {
         tipo: "video",
-        src: "https://youtu.be/B-BnUl1IK9I",
-        titulo: "Magic Words Song "
-    },
-    {
-        tipo: "video",
-        src: "https://youtu.be/nnB0sYRNzEw",
-        titulo: "Good Morning"
+        src: "youtube.com/watch?v=Z6c2aNlv4eQ",
+        titulo: "ABC Song"
     },
     {
         tipo: "video",
         src: "https://youtu.be/nnB0sYRNzEw",
         titulo: "Good Morning"
-    },
-    {
-        tipo:"video",
-        src:"https://youtu.be/vXXiyIGqliE?list=PLuA75Ir_S2pVePY1LjQpdsPxPIEOJzkZo",
-        titulo:"Body Parts Song"
-    },
-    {
-        tipo:"imagen",
-        src:"./img/image.png",  
-        titulo:"local"
     },
     {
         tipo:"imagen",
         src:"https://english4kidsonline.com/wp-content/uploads/2023/01/los-pronombres-en-ingles-768x480.jpg",  
         titulo:"web"
     },
-    {
-        tipo:"imagen",
-        src:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRB00G6EKGO3YgqqB9yY-RkOpJ0ZwYwdb6-uw&s",
-        titulo: "ingles"
-    },
-    {
-        tipo:"imagen",
-        src:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxcJrFfnSqFrgPRRQbJWQlku74rON9ugrimw&s",
-        titulo:"ingless"
-    },
+
     {
         tipo: "texto",
         texto: "sin informacion de clases de ingles"
