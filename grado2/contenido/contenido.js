0d2beed578f55b996c15ccb26c4a656fc668b90f
EnglishTeamXammar <d40592940p@perueduca.edu.pe>
2026-05-07 12:29:04 -0500

    Update contenido.js

--- grado2/contenido/contenido.js
+++ grado2/contenido/contenido.js
@@ -41,14 +41,15 @@ const contenido = [
         titulo:"What's Your Favorite Color?"
     },
     {
-        tipo:"imagen",
-        src:"./img/image.png",  
-        titulo:"local"
+        tipo:"video",
+        src:"https://www.youtube.com/watch?v=8ChQVaEAKsk",
+        titulo:"This Is My Face | Parts Of The Face Song!"
     },
+
     {
         tipo:"imagen",
         src:"https://english4kidsonline.com/wp-content/uploads/2023/01/los-pronombres-en-ingles-768x480.jpg",  
-        titulo:"web"
+        titulo:"subject"
     },
     {
         tipo:"imagen",
@@ -56,17 +57,12 @@ const contenido = [
         titulo: "ingles"
     },
     {
-        tipo:"imagen",
-        src:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxcJrFfnSqFrgPRRQbJWQlku74rON9ugrimw&s",
-        titulo:"ingless"
-    },
-    {
         tipo: "texto",
         texto: "sin informacion de clases de ingles"
     },
 ];
 
-// 🔥 convertidor YouTube
+//  convertidor YouTube
 function convertirYoutube(url) {
     if (url.includes("youtu.be")) {
         const id = url.split("/").pop();
@@ -82,13 +78,13 @@ function convertirYoutube(url) {
 }
 
 
-// 📦 contenedores (IMPORTANTE: coinciden con tu HTML)
+// contenedores (IMPORTANTE: coinciden con tu HTML)
 const contVideos = document.getElementById("videos");
 const contImagenes = document.getElementById("imagenes");
 const contTextos = document.getElementById("textos");
 
 
-// 🚀 render
+// render
 function renderContenido() {
 
     contVideos.innerHTML = "";
@@ -97,7 +93,7 @@ function renderContenido() {
 
     contenido.forEach(item => {
 
-        // 🎬 VIDEOS
+        //  VIDEOS
         if (item.tipo === "video") {
             contVideos.innerHTML += `
                 <div class="card">
